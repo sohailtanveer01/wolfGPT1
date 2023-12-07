44a19bd9731d44bb5b823125ee2d6b7d70902335
md sohail <sohail@mds-MacBook-Pro.local>
2023-12-07 17:10:39 -0600

    new wolf logo

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,7 +5,7 @@ import styles from "./home.module.scss";
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/github.svg";
-import ChatGptIcon from "../icons/wolfcolor.svg";
+import ChatGptIcon from "../icons/WWOLF.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
 import DeleteIcon from "../icons/delete.svg";
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          wolfGPT
+          WolfGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Intelligence. Delivered
